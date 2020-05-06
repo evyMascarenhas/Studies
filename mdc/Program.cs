b2964da43b642057d3bb519a94a2441f5076ab2a
Evellyn Mascarenhas <evellyn@outlook.com.br>
2020-05-06 16:47:24 -0300

    first commit

---
+++ mdc/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace mdc
+{
+    class Program
+    {
+        private  int GDC(int m, int n)
+        {
+            int i;
+
+            if (n == 0) return m;
+            return GDC(n, m % n);
+        }
+
+        public int GeneralizedGCD(int num, int[] arr)
+        {
+            // WRITE YOUR CODE HERE
+            int result = arr[0];
+            for (int i = 1; i < num; i++)
+            {
+                result = GDC(result, arr[i]);
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
